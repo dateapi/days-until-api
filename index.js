0960018320af9c6c7e55863e1603b13f69e74e33
dateapi <152834978+dateapi@users.noreply.github.com>
2023-12-04 16:25:55 +1100

    Initial commit

---
+++ index.js
@@ -0,0 +1,18 @@
+// index.js
+const express = require('express');
+const moment = require('moment-timezone');
+
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+app.get('/countdown/:date', (req, res) => {
+    const userDate = moment.tz(req.params.date, 'Asia/Manila');
+    const currentDate = moment.tz('Asia/Manila');
+    const daysUntil = userDate.diff(currentDate, 'days');
+
+    res.json({ daysUntil });
+});
+
+app.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`);
+});
