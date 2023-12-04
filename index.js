4929da471d720a6689761966b56065672d2fb02e
dateapi <152834978+dateapi@users.noreply.github.com>
2023-12-04 17:51:28 +1100

    Update index.js

--- index.js
+++ index.js
@@ -1,20 +1,19 @@
 const express = require('express');
 const moment = require('moment-timezone');
-
 const app = express();
-const port = 3000; // You can change this port if needed
+const port = process.env.PORT || 3000;
 
-// Define a route for the countdown
-app.get('/countdown', (req, res) => {
-  // Calculate the days until a specific date (replace '2023-12-31' with your target date)
-  const targetDate = moment.tz('2023-12-31', 'Your_Timezone');
-  const today = moment();
-  const daysUntil = targetDate.diff(today, 'days');
+app.get('/:date', (req, res) => {
+  const requestedDate = req.params.date;
+  const timeZone = 'Asia/Manila'; // Set your desired timezone
+
+  // Calculate the number of days until the requested date
+  const currentDate = moment().tz(timeZone);
+  const daysUntil = moment(requestedDate).tz(timeZone).diff(currentDate, 'days');
 
   res.json({ daysUntil });
 });
 
-// Start the server
 app.listen(port, () => {
-  console.log(`Server is running on http://localhost:${port}`);
+  console.log(`Server is running on port ${port}`);
 });
